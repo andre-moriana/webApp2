9b276ddc5f3a3993f0ff7df07e58cf19c2079fd8
André Moriana <andre.moriana@free.fr>
2026-01-02 01:19:35 +0100

    fix: Correct attachment URL construction for uploaded files

--- public/assets/js/groups-chat.js
+++ public/assets/js/groups-chat.js
@@ -84,13 +84,13 @@ function createMessageElement(message) {
         } else {
             // Pour tous les autres cas, construire l'URL correcte vers api.arctraining.fr
             if (originalUrl.startsWith('/uploads/')) {
-                attachmentUrl = 'https://api.arctraining.fr' + originalUrl;
-            } else if (originalUrl.startsWith('uploads/')) {
                 attachmentUrl = 'https://api.arctraining.fr/uploads/messages/' + originalUrl;
+            } else if (originalUrl.startsWith('uploads/')) {
+                attachmentUrl = 'https://api.arctraining.fr/' + originalUrl;
             } else {
                 // Si c'est juste un nom de fichier
                 attachmentUrl = 'https://api.arctraining.fr/uploads/messages/' + originalUrl;
-           }
+          }
         }
         
         attachmentHtml = `
