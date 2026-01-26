772d09495f1d553f3183f6455c3db9fe9c2e4891
Andre Moriana <andre.moriana@free.fr>
2026-01-26 14:36:16 +0100

    refactor: Clean up HTML structure in concours show view by removing unnecessary text in action buttons

--- app/Views/concours/show.php
+++ app/Views/concours/show.php
@@ -220,10 +220,9 @@ $isNature3DOrCampagne = isset($disciplineAbv) && in_array($disciplineAbv, ['3', 
                                 <td<?= $rowStyle ?>><?= htmlspecialchars($inscription['created_at'] ?? $inscription['date_inscription'] ?? 'N/A') ?></td>
                                 <td<?= $rowStyle ?>>
                                     <a href="/concours/<?= htmlspecialchars($concours->id ?? $concours->_id ?? '') ?>/inscription" class="btn btn-sm btn-primary me-1">
-                                        <i class="fas fa-edit"></i> Éditer
-                                    </a>
+                                        <i class="fas fa-edit"></i>                                     </a>
                                     <button type="button" class="btn btn-sm btn-danger" onclick="removeInscription(<?= htmlspecialchars($inscription['id'] ?? '') ?>)">
-                                        <i class="fas fa-trash"></i> Retirer
+                                        <i class="fas fa-trash"></i>
                                     </button>
                                 </td>
                             </tr>
