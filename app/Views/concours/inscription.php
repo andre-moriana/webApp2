40f879351daeebcf3cf1f6869ce730f621fc54b1
Andre Moriana <andre.moriana@free.fr>
2026-02-03 18:50:10 +0100

    feat: Ajouter une vérification pour déterminer si un plan de cible est nécessaire pour les disciplines S, T, I, H

--- app/Views/concours/inscription.php
+++ app/Views/concours/inscription.php
@@ -384,6 +384,10 @@ table tbody tr.piquet-blanc {
                         <?php endif; ?>
                     </div>
                     
+                    <?php 
+                    // Vérifier si le concours nécessite un plan de cible (disciplines S, T, I, H)
+                    $needsPlanCible = isset($disciplineAbv) && in_array($disciplineAbv, ['S', 'T', 'I', 'H'], true);
+                    ?>
                     <?php if ($needsPlanCible): ?>
                         <!-- Sélection de cible et position dans le plan de cible -->
                         <div class="row mt-3" id="plan-cible-selection" style="display: none;">
