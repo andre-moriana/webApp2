cab2b965131abc18df23f5e3bca50f3d86fd8c91
Andre Moriana <andre.moriana@free.fr>
2026-02-26 16:26:56 +0100

    feat: Refactor Facebook feed integration in club feed view
    - Replaced the Facebook page blockquote with an iframe for improved rendering and compatibility.
    - Adjusted the minimum height of the Facebook feed wrapper for better layout.
    - Removed unnecessary JavaScript for Facebook SDK initialization, simplifying the code.

--- app/Views/club-feed/index.php
+++ app/Views/club-feed/index.php
@@ -41,39 +41,33 @@ $clubName = $clubName ?? 'votre club';
                         <a href="<?php echo htmlspecialchars($fbHref); ?>" target="_blank" rel="noopener noreferrer" class="btn btn-outline-primary btn-sm mb-3">
                             <i class="fab fa-facebook me-1"></i> Voir la page Facebook dans un nouvel onglet
                         </a>
-                        <div class="fb-feed-wrapper" style="min-height: 500px;">
-                            <div id="fb-root"></div>
-                            <div class="fb-page" 
-                                 data-href="<?php echo htmlspecialchars($fbHref); ?>" 
-                                 data-tabs="timeline" 
-                                 data-width="500" 
-                                 data-height="700" 
-                                 data-small-header="false" 
-                                 data-adapt-container-width="true" 
-                                 data-hide-cover="false" 
-                                 data-show-facepile="true">
-                                <blockquote cite="<?php echo htmlspecialchars($fbHref); ?>" class="fb-xfbml-parse-ignore">
-                                    <a href="<?php echo htmlspecialchars($fbHref); ?>"><?php echo htmlspecialchars($clubName); ?> sur Facebook</a>
-                                </blockquote>
-                            </div>
+                        <div class="fb-feed-wrapper" style="min-height: 400px;">
+                            <?php
+                            $iframeSrc = 'https://www.facebook.com/plugins/page.php?' . http_build_query([
+                                'href' => $fbHref,
+                                'tabs' => 'timeline',
+                                'width' => '500',
+                                'height' => '700',
+                                'small_header' => 'false',
+                                'adapt_container_width' => 'true',
+                                'hide_cover' => 'false',
+                                'show_facepile' => 'true',
+                                'locale' => 'fr_FR',
+                            ]);
+                            ?>
+                            <iframe src="<?php echo htmlspecialchars($iframeSrc); ?>"
+                                    width="500"
+                                    height="700"
+                                    style="border:none;overflow:hidden;max-width:100%;"
+                                    scrolling="no"
+                                    frameborder="0"
+                                    allowfullscreen="true"
+                                    allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                                    title="Page Facebook <?php echo htmlspecialchars($clubName); ?>">
+                            </iframe>
                         </div>
                     </div>
                 </div>
-                <script>
-                    (function(d, s, id) {
-                        var js, fjs = d.getElementsByTagName(s)[0];
-                        if (d.getElementById(id)) return;
-                        js = d.createElement(s); js.id = id;
-                        js.src = "https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v18.0";
-                        js.async = true; js.defer = true;
-                        fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));
-                    window.fbAsyncInit = function() {
-                        if (typeof FB !== 'undefined') {
-                            FB.XFBML.parse();
-                        }
-                    };
-                </script>
             <?php endif; ?>
         </div>
     </div>
